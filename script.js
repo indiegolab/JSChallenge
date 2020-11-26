02c4ebed2f66fb7c2344ad11e6e9e1cf4ffb1ce5
Always Floating <40168104+indiegolab@users.noreply.github.com>
2020-11-27 00:06:17 +0900

    init.

---
+++ script.js
@@ -0,0 +1,37 @@
+// alert("I'm working.")
+console.log('I do working')
+
+/* Arrys = List*/ 
+const daysOfWeek = ["Mon","Tue","Wed","Thu","Fri"]
+console.log(daysOfWeek)
+console.log(daysOfWeek[2])
+
+// Object 선언 = 중괄호
+const nicoInfo = {
+  name: "Nico",
+  age: "22",
+  gender: "Male",
+  isHansome: true,
+  favMovies: ["Inception", "Dark knight"],
+  favFood: [
+    {
+      name: "kimchi", fatty: false
+    },
+    {
+      name: "pizza", fatty: true
+    }
+  ]
+}
+
+
+console.log(nicoInfo)
+console.log(nicoInfo.name)
+
+// const 안에 있는 값을 바꿀 수 있다.
+nicoInfo.isHansome = false
+console.log(nicoInfo.isHansome)
+
+
+console.log('Hello')
+console.log(console)
+console.log(nicoInfo,console)
